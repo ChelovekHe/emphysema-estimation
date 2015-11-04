ee2582885a6e13e00998fcc1efb636b6fff686d2
Silas Ørting <silas.orting@gmail.com>
2015-11-04 08:37:21 +0100

    New version of Dataset-2 script

--- Scripts/MakeDataset-2.py
+++ Scripts/MakeDataset-2.py
@@ -1,143 +1,64 @@
 #!/usr/bin/python3
 '''See ../Data/Dataset-2/README.md'''
 
-import sys, os, os.path, csv, glob, subprocess, re, math
+import sys, os, os.path, subprocess, math, csv
+from Util import intersperse 
 
 def main():
-    dataset0Dir = '../Data/Dataset-0'
-    dataset2Dir = '../Data/Dataset-2'
+    basedir = '../Data/Dataset-2'
 
-    instancesFile = os.path.join( dataset2Dir, 'instances.csv' )
+    dirs = {
+        'Bags' : os.path.join(basedir, 'Bags'),
+        'Histograms' : os.path.join(basedir, 'Histograms'),
+        'FileLists' : os.path.join(basedir, 'FileLists'),
+        'Bin' : '../Build'
+    }
     
-    maskDir = os.path.join(dataset0Dir, 'Lungs')
-    featureDir = os.path.join(dataset0Dir, 'Features')
-    fileListDir = os.path.join(dataset2Dir, 'FileLists')
-    roiDir = os.path.join(dataset2Dir, 'ROIs')
-    histogramDir = os.path.join(dataset2Dir, 'Histograms')
-
-    imagesMaskListPath = os.path.join(fileListDir, 'FeaturesMaskList.csv')
-    roiMaskListPath = os.path.join(fileListDir, 'ROIMaskList.csv')
-
-    roiExtractionScript = './RunROIExtraction.py'
-    maskedHistogramScript = './RunMaskedHistograms.py'
-
-    numROIs = 50
-    roiSize = 41
-    numBins = 41
-    numSamples = 10000
-    scales = ["%0.2f" % (0.6 * math.sqrt(2.0)**i) for i in range(7)]
+    files = {
+        'ScanMaskList' : os.path.join(dirs['FileLists'], 'ScanMaskList.csv'),
+        'HistogramSpec' : os.path.join(dirs['Histograms'], 'Dataset-3-HistogramSpec.txt'),
+    }
     
-    # Make the images-mask list
-    print('Creating images-mask list')
-    intensity = sorted(glob.glob( os.path.join( featureDir, '*masked*' ) ))
-    gradientMagnitude = sorted(glob.glob( os.path.join( featureDir, '*GradientMagnitude*' ) ))
-    eig1 = sorted(glob.glob( os.path.join( featureDir, '*eig1*' ) ))
-    eig2 = sorted(glob.glob( os.path.join( featureDir, '*eig2*' ) ))
-    eig3 = sorted(glob.glob( os.path.join( featureDir, '*eig3*' ) ))
-    LoG = sorted(glob.glob( os.path.join( featureDir, '*LoG*' ) )) 
-    Curvature = sorted(glob.glob( os.path.join( featureDir, '*Curvature*' ) ))
-    Frobenius = sorted(glob.glob( os.path.join( featureDir, '*Frobenius*' ) ))
+    progs = {
+        'MakeBag' : os.path.join(dirs['Bin'],'FeatureExtraction/MakeBag'),
+    }
 
-    # We have features at multiple scales, but the mask is only at one scale
-    # so we need to repeat it
-    mask = glob.glob( os.path.join( maskDir, '*lungs*' ) )
-    scanNames = [os.path.basename(s)[:-14] for s in mask]
-    repeatMask = int(max(1, len(intensity)/len(mask)))
-    mask = sorted( mask * repeatMask )
+    params = {
+        'scales' : ["%0.2f" % (0.6 * math.sqrt(2.0)**i) for i in range(7)],
+        'num-rois' : 50,
+        'roi-size-x' : 41,
+        'roi-size-y' : 41,
+        'roi-size-z' : 41,
+    }
     
-    imagesMaskList = zip( intensity,
-                          gradientMagnitude,
-                          eig1,
-                          eig2,
-                          eig3,
-                          LoG,
-                          Curvature,
-                          Frobenius,
-                          mask
-                      )
-    
-    with open(imagesMaskListPath, 'w') as out:
-        csv.writer( out ).writerows(imagesMaskList)
-
-    # Run ROI extraction
-    # This creates a lot of ROIs that need to be grouped according to which
-    # image they belong to.
-    print('Running ROI extraction')
-    args = [
-        roiExtractionScript,
-        imagesMaskListPath,
-        roiDir,
-        "%d" % numROIs,
-        "%d" % roiSize,
-    ]
+    # Make the bags
+    print( 'Making bags' )
+    with open(files['ScanMaskList']) as infile:
+        scanMaskList = [(row[0].strip(), row[1].strip()) for row in csv.reader(infile)]
         
-    if subprocess.call( args ) != 0:
-        print( 'Error running ROI extraction' )
-        return 1
+    for scan, mask in scanMaskList:
+        print( "Using scan '%s'" % scan )
+        print( "Using mask '%s'" % mask )
+        args = [
+            progs['MakeBag'],
+            '--image', scan,
+            '--mask', mask,
+            '--histogram-spec', files['HistogramSpec'],
+            '--outdir', dirs['Bags'],
+            '--prefix', os.path.basename(scan),
+            '--num-rois', '%d' % params['num-rois'],
+            '--roi-size-x', '%d' % params['roi-size-x'],
+            '--roi-size-y', '%d' % params['roi-size-y'],
+            '--roi-size-z', '%d' % params['roi-size-z'],
+        ] + list(intersperse('--scale', (s for s in params['scales'])))
+        
+        print(' '.join(args))        
+        if subprocess.call( args ) != 0:
+            print( 'Error making bag' )
+            return 1
 
-    # Make the ROI/mask list
-    print( "Creating ROI/mask list for each feature and calculating histograms." )
-    featureNames = [
-        'masked',
-        'GradientMagnitude',
-        'eig1',
-        'eig2',
-        'eig3',
-        'LoG',
-        'Curvature',
-        'Frobenius'
-    ]
-
-    maskPattern = os.path.join( roiDir, '*lungs*nii.gz' )
-    maskPaths = sorted(glob.glob( maskPattern ))
-    for featureName in featureNames:
-        for scale in scales:
-            pattern = os.path.join( roiDir, '*scale_' + scale + '*' + featureName + '*')
-            paths = sorted(glob.glob( pattern ) )
-            if len(paths) > 0:
-                fileListPath = os.path.join(fileListDir, 'ROI' + featureName + '_' + scale + '.csv')
-                try:
-                    with open( fileListPath, 'w' ) as out:
-                        csv.writer( out ).writerows( zip(paths, maskPaths) )
-                except Exception as e:
-                    print(e)
-                print( fileListPath )
-                args = [
-                    maskedHistogramScript,
-                    fileListPath,
-                    histogramDir,
-                    "%d" % numBins,
-                    "%d" % numSamples
-                ]
-                if subprocess.call(args) != 0:
-                    print( 'Error creating histograms' )
-
-    # Now we need to concatenate the histogram responses into a matrix of
-    # ROIs and histograms
-    # A ROI is identified by the scan name and the ROI number.
-    print( 'Collating histogram into feature matrix' )
-    instances = []
-    for scanName in scanNames:
-        for roi in range(numROIs):
-            instance = []
-            for featureName in featureNames:
-                for scale in scales:
-                    path = '%s_scale_%s0000_%s_ROI_%05d_hist.txt' % (scanName, scale, featureName, roi)
-                    with open(os.path.join(histogramDir, path)) as f:
-                        for row in csv.reader( f ):
-                            for elem in row:
-                                instance.append( float(elem) )
-            instances.append(instance)
-            
-    with open( instancesFile, 'w' ) as out:
-        csv.writer( out ).writerows( instances )    
     
     return 0
 
-
 if __name__ == '__main__':
     sys.exit( main() )
-
-
-
-    
