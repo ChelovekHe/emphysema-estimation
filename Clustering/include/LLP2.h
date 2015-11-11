735bc4f290883f0013d55b741f29b739ed565f40
Silas Ørting <silas.orting@gmail.com>
2015-11-11 13:45:02 +0100

    LLP implementation with CMA-ES optimization

---
+++ Clustering/include/LLP2.h
@@ -0,0 +1,79 @@
+#ifndef __LLP2_h
+#define __LLP2_h
+
+#include <vector>
+
+#include "Eigen/Dense"
+
+#include "MatrixOperations.h"
+#include "ClusterLabeller.h"
+#include "WeightedEarthMoversDistance.h"
+#include "KMeansClusterer.h"
+
+class LLP2 {
+public:
+  typedef Eigen::Matrix<double, Eigen::Dynamic, 1> VectorType;
+  typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixType;
+
+  typedef ClusterLabeller<MatrixType, VectorType> LabellerType;
+
+  typedef WeightedEarthMoversDistance< double > DistanceFunctorType;
+  typedef KMeansClusterer< DistanceFunctorType > ClustererType;
+
+  typedef std::vector< std::size_t > IndexVectorType;
+
+  /**
+     Structure holding the result of running the LLP2 optimization
+   */
+  struct ResultType {
+    typename ClustererType::ResultType clustering;
+    MatrixType C;
+    VectorType labels;
+    double error;
+  };
+
+  /**
+     @param p          Bag label proportions
+     @param instances  Instance matrix of dimension |instances| x |features|
+     @param bagIndices Bag indices matching each instance to a bag in p
+     @param k          Maximum number of clusters to use for clustering
+   */
+  LLP2( const VectorType& p,
+	const MatrixType& instances,
+	const IndexVectorType& bagIndices,
+	unsigned int k,
+	std::size_t nHistograms,
+	std::size_t nBins
+	);
+
+  ~LLP2() {};
+
+  /**
+     Run the label proportion learning and return the error.
+     
+     @param weights  Weights for the distance function.
+     @return         The error measure for the given weights.
+  */
+  double operator()( const double* weights, const int& N );
+  
+  /**
+     Get the result of the optmization.
+     
+     @return  Struct holding clustering + error.
+   */
+  ResultType getResult(  );
+
+private:
+  const MatrixType& m_Instances;
+  const IndexVectorType& m_BagIndices;
+  ClustererType m_Clusterer;
+  LabellerType m_Labeller;
+  std::size_t m_NumberOfBags;
+  unsigned int m_K;
+  ResultType m_Result;
+  DistanceFunctorType m_Dist;
+};
+
+#include "LLP2.hxx"
+
+#endif
---
+++ Clustering/include/LLP2.hxx
@@ -0,0 +1,75 @@
+#ifndef __LLP2_hxx
+#define __LLP2_hxx
+
+#include "LLP2.h"
+
+LLP2
+::LLP2( const VectorType& p,
+	const MatrixType& instances,
+	const IndexVectorType& bagIndices,
+	unsigned int k,
+	std::size_t nHistograms,
+	std::size_t nBins
+       )
+  : m_Instances( instances ),
+    m_BagIndices( bagIndices ),
+    m_Clusterer( 2, -1 ),
+    m_Labeller( p ),
+    m_NumberOfBags( p.size() ),
+    m_K( k ),
+    m_Result( ),
+    m_Dist( std::vector< std::pair< unsigned int, double > >( nHistograms, {nBins, 1} ))
+{
+  
+  // We must have that bagIndices can be used to index into p
+  auto maxI = std::max_element( bagIndices.cbegin(), bagIndices.cend()); 
+  if( maxI != bagIndices.end() && *maxI >= p.size() ) {
+    throw std::invalid_argument( "Largest bag index is to large" );
+  }
+  
+  // We must have that each instance has a bag label
+  if ( instances.rows() != bagIndices.size() ) {
+    throw std::invalid_argument( "There must exactly one bag index for each instance" );
+  }
+
+  // We must have that the number of features match number of bins in histograms
+  if ( instances.cols() != nHistograms*nBins ) {
+    throw std::invalid_argument( "instances.cols() != nHistograms*nBins" );
+  }
+}
+
+double
+LLP2
+::operator()( const double* featureWeights, const int& N ) {
+
+  m_Dist.setWeights( featureWeights, N );
+
+  // Find clusters
+  m_Result.clustering = m_Clusterer.cluster( m_Instances, m_Dist, m_K );
+
+  // Find mapping from clusters to bags
+  const std::size_t k = m_Result.clustering.centers.rows();
+
+  assert( m_BagIndices.size() == m_Result.clustering.indices.size() );
+  MatrixType C = MatrixType::Zero( m_NumberOfBags, k );
+  coOccurenceMatrix( m_BagIndices.begin(),
+		     m_BagIndices.end(),
+		     m_Result.clustering.indices.begin(),
+		     m_Result.clustering.indices.end(),
+		     C );
+  m_Result.C = rowNormalize(C);
+  // Find labels 
+  m_Result.labels = VectorType::Zero( k );
+  m_Result.error = m_Labeller.findOptimalLabels( m_Result.C,
+						 m_Result.labels );
+
+  return m_Result.error;
+}
+
+LLP2::ResultType
+LLP2
+::getResult() {
+  return m_Result;
+}
+
+#endif
