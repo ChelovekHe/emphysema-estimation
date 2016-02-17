7d33f9c858c9088907d72d6757f0491b03cfdd34
Silas Ørting <silas.orting@gmail.com>
2016-02-17 10:47:55 +0100

    Rewrite of cost functions

--- Optimization/include/LLPCostFunction2.h
+++ Optimization/include/LLPCostFunction2.h
@@ -1,16 +1,15 @@
-#ifndef __LLPCostFunction_h
-#define __LLPCostFunction_h
+#ifndef __LLPCostFunction2_h
+#define __LLPCostFunction2_h
 
 #include "ceres/ceres.h"
 /**
    Cost function implementing
    r_i(x) = p_i - (Cx)_i
-   r_{N+1} = \lambda * sum_i r_i(x)
 
    See: ClusterLabeller.h
 */
 template< typename TVector, typename TMatrix >
-class LLPCostFunction : public ceres::CostFunction {
+class LLPCostFunction2 : public ceres::CostFunction {
 public:
   typedef TVector VectorType;
   typedef TMatrix MatrixType;
@@ -19,40 +18,38 @@ public:
      @param p \in [0,1]^N is the known bag label proportions
      @param C \in R^{N \times K} is a mapping from cluster labels to bag labels
   */
-  LLPCostFunction(VectorType p, MatrixType C, double lambda=1.0)
-    : m_P(p), m_C(C), m_Lambda(lambda)
+  LLPCostFunction2(VectorType p, MatrixType C)
+    : m_P(p), m_C(C)
   {
     assert( p.size() == C.rows() );
-    assert( lambda > 0 );
 
     // We have K parameters
     auto* sizes = mutable_parameter_block_sizes();
     sizes->push_back( C.cols() );
 
-    // We have N + 1 residuals.
-    set_num_residuals( static_cast<int>(p.size()) + 1 );
+    // We have N residuals.
+    set_num_residuals( static_cast<int>(p.size()) );
   }
 
-  virtual ~LLPCostFunction() {}
+  virtual ~LLPCostFunction2() {}
 
   /**
      @parameters Array of length 1 containing a pointer to an array of K 
                  parameters.
-     @residuals  Array of length N+1.
-     @jacobians  Array of length 1 containing a pointer to an array of N+1 
+     @residuals  Array of length N.
+     @jacobians  Array of length 1 containing a pointer to an array of N
                  Jacobian vectors of length K.
    */
   bool Evaluate(double const* const* parameters,
 		double* residuals,
 		double** jacobians) const {
-    // We need to calculate p - Cx and sum_i (p - Cx)_i
+    // We need to calculate p - Cx
 
     // We have one parameter block, with K parameters
     double const* x = parameters[0];
       
     const std::size_t N = m_C.rows();
     const std::size_t K = m_C.cols();
-    residuals[N] = 0;
     for ( std::size_t i = 0; i < N; ++i ) {
       // Compute y = (C*x)_i
       double y = 0; 
@@ -60,10 +57,7 @@ public:
 	y += m_C(i,j)* x[j]; 
       }
       residuals[i] = m_P(i) - y;
-      residuals[N] += m_P(i) - y;
     }
-
-    residuals[N] *= m_Lambda;
 
     if ( jacobians != NULL && jacobians[0] != NULL ) {
       // Compute the Jacobian
@@ -74,15 +68,6 @@ public:
 	for ( std::size_t j = 0; j < K; ++j ) {
 	  // Jacobian for residual i and parameters j
 	  J[i * K + j] = - m_C(i,j);
-
-	  // Add to the derivative of the last term, which is the sum of all
-	  // the residual derivatives for parameter j, multiplied by lambda
-	  if ( i == 0 ) {
-	    J[N * K + j] = - m_Lambda*m_C(i,j);
-	  }
-	  else {
-	    J[N * K + j] -= m_Lambda*m_C(i,j);
-	  }
 	}
       }
     }
@@ -92,7 +77,6 @@ public:
 private:
   VectorType m_P;
   MatrixType m_C;
-  double m_Lambda;
 };
 
 #endif
